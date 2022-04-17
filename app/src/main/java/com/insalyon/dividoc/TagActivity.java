24c543579a22ce7622f3a0913a9790c4776a04b8
user444 <user444@localhost.localdomain>
2022-04-17 18:29:32 -0400

    Basic TagActivity implementation

--- app/src/main/java/com/insalyon/dividoc/TagActivity.java
+++ app/src/main/java/com/insalyon/dividoc/TagActivity.java
@@ -1,6 +1,29 @@
 package com.insalyon.dividoc;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag);
+
+        // Spinners
+        Spinner gender_spinner = (Spinner) findViewById(R.id.gender_spinner);
+        ArrayAdapter<CharSequence> gender_adapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
+        gender_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gender_spinner.setAdapter(gender_adapter);
+
+        Spinner age_spinner = (Spinner) findViewById(R.id.age_spinner);
+        ArrayAdapter<CharSequence> age_adapter = ArrayAdapter.createFromResource(this, R.array.age, android.R.layout.simple_spinner_item);
+        age_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        age_spinner.setAdapter(age_adapter);
+
+    }
 }

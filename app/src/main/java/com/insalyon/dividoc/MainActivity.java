c29f066e915adfed4b51bd393513c021b7010851
Armand31 <armandprioreschi@yahoo.fr>
2022-04-18 20:21:27 -0400

    Implementing InitActivity

--- app/src/main/java/com/insalyon/dividoc/MainActivity.java
+++ app/src/main/java/com/insalyon/dividoc/MainActivity.java
@@ -3,33 +3,52 @@ package com.insalyon.dividoc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addNewCaseButton;
+    private Button exportFilesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Button listeners
-        Button add_new_case_button = (Button) findViewById(R.id.add_new_case);
-        add_new_case_button.setOnClickListener(new View.OnClickListener() {
+        // If this is the first time the user is using the app, he has to input his serial number
+        // via the InitActivity
+        SharedPreferences preferences = getSharedPreferences("Preferences", MODE_PRIVATE);
+        if (!preferences.contains("FirstStart")) {
+            startActivity(new Intent(MainActivity.this, InitActivity.class));
+        }
+
+        setButtonListeners();
+    }
+
+    /**
+     * Set button listeners for "Add a new case" and "Export files" buttons, which will trigger
+     * new activities
+     */
+    private void setButtonListeners() {
+        // Start TagActivity if the "Add a new case" button is clicked
+        Button addNewCaseButton = (Button) findViewById(R.id.add_new_case);
+        addNewCaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, TagActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, TagActivity.class));
             }
         });
 
-        Button export_files_button = (Button) findViewById(R.id.export_file_button);
-        export_files_button.setOnClickListener(new View.OnClickListener() {
+        // Start ExportActivity if the "Export files" button is clicked
+        Button exportFilesButton = (Button) findViewById(R.id.export_file_button);
+        exportFilesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ExportActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, ExportActivity.class));
             }
         });
     }
